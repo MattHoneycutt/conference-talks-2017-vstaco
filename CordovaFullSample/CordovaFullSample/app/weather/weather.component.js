2305b347c6d433c4da8fc14be555141ddc5a0496
MattHoneycutt <mbhoneycutt@gmail.com>
2017-05-16 16:02:37 -0500

    Initial commit.

---
+++ CordovaFullSample/CordovaFullSample/app/weather/weather.component.js
@@ -0,0 +1,33 @@
+﻿(function (undefined) {
+
+    angular.module('app')
+        .component('weather',
+        {
+            templateUrl: '/app/weather/weather.template.html',
+            controller: controller,
+            controllerAs: 'vm'
+        });
+
+    function controller($http) {
+        const vm = this;
+
+        //TODO: Substitue this with your own openweathermap.org API key.
+        const apiKey = 'API_KEY';
+
+        vm.$init = $init();
+
+        function $init() {
+            const zipcode = '37115';
+            const url = 'http://api.openweathermap.org/data/2.5/weather?' +
+                `zip=${zipcode},us&appid=${apiKey}&units=imperial`;
+
+            $http.get(url)
+                .then(resp => {
+                    vm.weather = resp.data;
+                })
+                .catch(ex => {
+                    vm.ex = JSON.stringify(ex);
+                });
+        }
+    }
+})();
